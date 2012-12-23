825e451bb754bc23b13d83e7f85c30a34ffc0a7a
Bo Anderson <bo@toxicflames.co.uk>
2012-12-23 21:35:09 +0000

    Use NBT for compatibility with YMLs created in older versions.

--- src/main/java/com/mtihc/minecraft/treasurechest/v8/core/ItemStackWrapper.java
+++ src/main/java/com/mtihc/minecraft/treasurechest/v8/core/ItemStackWrapper.java
@@ -7,7 +7,11 @@ import java.util.List;
 import java.util.Map;
 
 import net.minecraft.server.v1_4_6.Item;
+import net.minecraft.server.v1_4_6.NBTTagCompound;
+import net.minecraft.server.v1_4_6.NBTTagList;
+import net.minecraft.server.v1_4_6.NBTTagString;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.craftbukkit.v1_4_6.inventory.CraftItemStack;
@@ -45,16 +49,14 @@ public class ItemStackWrapper implements ConfigurationSerializable {
 		Map<?, ?> extra = (Map<?, ?>) values.get("tag");
 		if(extra != null) {
 			CraftItemStack craftItem = CraftItemStack.asCraftCopy(stack);
+			NBTTagCompound tag = new NBTTagCompound("tag");
 			if(stack.getType() == Material.WRITTEN_BOOK || stack.getType() == Material.BOOK_AND_QUILL) {
-				BookMeta meta = (BookMeta)craftItem.getItemMeta();
-				writtenBook(meta, extra);
-				stack = craftItem;
-				craftItem.setItemMeta(meta);
+				net.minecraft.server.v1_4_6.ItemStack nmsItem = CraftItemStack.asNMSCopy(craftItem);
+				writtenBook(tag, extra);
+				nmsItem.setTag(tag);
+				stack = CraftItemStack.asCraftMirror(nmsItem);
 			}
 		}
-		
-		
-		
 	}
 
 	@Override
@@ -62,25 +64,29 @@ public class ItemStackWrapper implements ConfigurationSerializable {
 		Map<String, Object> values = new LinkedHashMap<String, Object>();
 		values.put("stack", stack);
 		
-		BookMeta meta = (BookMeta)stack.getItemMeta();
-		if (meta == null) {
+		net.minecraft.server.v1_4_6.ItemStack nmsStack = CraftItemStack.asNMSCopy(stack);
+		NBTTagCompound tag = nmsStack.getTag();
+		if (tag == null) {
 			return values;
 		}
+		
+		Bukkit.getLogger().info("tag name: " + tag.getName());
 		
 		if(stack.getType() == Material.WRITTEN_BOOK || stack.getType() == Material.BOOK_AND_QUILL) {
 			Map<String, Object> extra = new LinkedHashMap<String, Object>();
 			
-			extra.put("author", meta.getAuthor());
-			extra.put("title", meta.getTitle());
+			extra.put("author", tag.getString("author"));
+			extra.put("title", tag.getString("title"));
 			
 			LinkedHashMap<String, Object> pageSection = new LinkedHashMap<String, Object>();
 			extra.put("pages", pageSection);
-			List<String> pages = meta.getPages();
+			NBTTagList pages = tag.getList("pages");
 			
 			int size = pages.size();
 			int index = 0;
 			while(index < size) {
-				pageSection.put("page" + index, pages.get(index));
+				NBTTagString page = (NBTTagString) pages.get(index);
+				pageSection.put("page" + index, page.data);
 				index++;
 			}
 			
@@ -90,22 +96,20 @@ public class ItemStackWrapper implements ConfigurationSerializable {
 		return values;
 	}
 	
-	
-	
-	
-
-	
-	private void writtenBook(BookMeta meta, Map<?, ?> values) {
-		meta.setAuthor((String) values.get("author"));
-		meta.setTitle((String) values.get("title"));
+	private void writtenBook(NBTTagCompound tag, Map<?, ?> values) {
+		tag.setString("author", (String) values.get("author"));
+		tag.setString("title", (String) values.get("title"));
+		
+		NBTTagList pageListTag = new NBTTagList("pages");
 		
 		Map<?, ?> pageSection = (Map<?, ?>) values.get("pages");
-		ArrayList<String> pages = new ArrayList<String>();
+		int index = 0;
 		Collection<?> pageValues = pageSection.values();
 		for (Object page : pageValues) {
-			pages.add(page.toString());
+			pageListTag.add(new NBTTagString(String.valueOf(index), page.toString()));
+			index++;
 		}
 		
-		meta.setPages(pages);
+		tag.set("pages", pageListTag);
 	}
 }
